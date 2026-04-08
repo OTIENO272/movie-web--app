5e3357b803a1bc9fae65c50cfca8d15416a88ad9
Victor Otieno <otienov272@gmail.com>
2026-04-08 18:24:24 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-bundle' // or '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base: "/movie-web--app/"
-
+  // Add this line below:
+  base: '/movie-web--app/', 
 })

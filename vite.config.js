c7728db43d16a15618af4caff6ad578523b1c994
Boateng@37 <otienov272@gmail.com>
2026-04-10 14:38:34 +0300

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-bundle' // or '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
-

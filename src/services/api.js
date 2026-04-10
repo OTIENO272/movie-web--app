211a5660a3b3b2d9d0700baac238d8aa324017fe
Victor Otieno <otienov272@gmail.com>
2026-04-10 15:28:32 +0300

    Fix async handling in searchMovies function

--- src/services/api.js
+++ src/services/api.js
@@ -12,6 +12,6 @@ export const  getPopularMovies = async() => {
 
 export const searchMovies = async (query) =>{
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data = response.json();
-    return data.results;
+    const data =await response.json();
+    return data.results || [];
 }
